98e3b463a9871dc5e2585185ddf3ec091dfca497
jjcc <jjccforth@gmail.com>
2019-05-10 19:02:46 +0000

    added switch to AAFC internal handler

--- ckanext/scheming/fanstatic/aafc.js
+++ ckanext/scheming/fanstatic/aafc.js
@@ -1,10 +1,19 @@
 /***
 place holder
 */
+function switchToInternal(){
+        $("#group-Open-Government-Dataset-Release-Information").addClass("hidden");
+        $("input#field-jurisdiction").val("N/A");
+        $("input#field-access_restriction").val("N/A");
+        $("select#field-access_to_information") .val("false");  
+        console.log("switched to internal");
+}
+
 function conditionalShow(){
 	var selected = $(this).val();
 	if(selected == "internal")
-          $("#group-Open-Government-Dataset-Release-Information").addClass("hidden");
+          //$("#group-Open-Government-Dataset-Release-Information").addClass("hidden");
+	  switchToInternal();
 	else
           $("#group-Open-Government-Dataset-Release-Information").removeClass("hidden");
 
