12e72bffd5bdc7cca1a9abd558408c70353c88c9
jjcc <jjccforth@gmail.com>
2019-08-12 18:13:34 +0000

    use the function for building child dropdown

--- ckanext/scheming/fanstatic/dynamicDropdown.js
+++ ckanext/scheming/fanstatic/dynamicDropdown.js
@@ -30,7 +30,6 @@ $('ul.mdpd').each(
            var id = data.id;
            var label = data.innerText;
            //dic[parent].push({"value":id,"lable":label});
-           //dic[parent].push(label);
            dic[parent].push({id:label});
         });
     }
@@ -46,14 +45,7 @@ $("#field-program_alignment_architecture_to_drf_core_responsibilities").change(
     function(){
         $("#field-drf_program_inventory").empty();
         var parent = $(this).children("option:selected").val();
-        var children = paa2drfData[parent];
-        children.forEach ( function(item) {
-            var val = Object.keys(item)[0];
-            var label = item[val];
-            $("#field-drf_program_inventory").append(
-                $('<option></option>').val(val).html(label)
-            );
-        })
+	buildChildrenDropdown(parent);
     }
 );
 
