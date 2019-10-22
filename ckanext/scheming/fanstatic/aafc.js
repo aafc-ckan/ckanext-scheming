2325186dac51c257a2f4ae73edb6dfacdb4dabab
jjcc <jjccforth@gmail.com>
2019-10-22 17:04:13 +0000

    added date initialization

--- ckanext/scheming/fanstatic/aafc.js
+++ ckanext/scheming/fanstatic/aafc.js
@@ -1,12 +1,21 @@
 /***
 place holder
 */
+function getToday(){
+	var now = new Date(); 
+	var day = ("0" + now.getDate()).slice(-2);
+	var month = ("0" + (now.getMonth() + 1)).slice(-2);
+	return  now.getFullYear()+"-"+(month)+"-"+(day) ;
+}
+
+
 function switchToInternal(){
         $("#group-Open-Data-Release-Criteria").addClass("hidden");
         $("input#field-jurisdiction").val("N/A");
         $("input#field-access_restriction").val("N/A");
         $("select#field-access_to_information").val("false");
 	$("select#field-license_id").val("aafc-dsa");
+	$("#field-date_published").val(getToday())
         console.log("switched to internal");
 }
 
@@ -16,6 +25,7 @@ function switchToOpenGovernment(){
         $("input#field-access_restriction").val("");
         $("select#field-access_to_information").val("");
 	$("select#field-license_id").val("");
+	$("#field-date_published").val(getToday())
         console.log("switched to open gov");
 }
 
