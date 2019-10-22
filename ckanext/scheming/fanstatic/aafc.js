5eb51daad2f4d6c67cb60446245e126154900659
Alexis S <alexis.small@canada.ca>
2019-10-22 13:12:44 -0400

    hide internal license from options

--- ckanext/scheming/fanstatic/aafc.js
+++ ckanext/scheming/fanstatic/aafc.js
@@ -14,8 +14,8 @@ function switchToInternal(){
         $("input#field-jurisdiction").val("N/A");
         $("input#field-access_restriction").val("N/A");
         $("select#field-access_to_information").val("false");
-	$("select#field-license_id").val("aafc-dsa");
-	$("#field-date_published").val(getToday())
+		$("select#field-license_id").val("aafc-dsa");
+		$("#field-date_published").val(getToday())
         console.log("switched to internal");
 }
 
@@ -24,8 +24,9 @@ function switchToOpenGovernment(){
         $("input#field-jurisdiction").val("");
         $("input#field-access_restriction").val("");
         $("select#field-access_to_information").val("");
-	$("select#field-license_id").val("");
-	$("#field-date_published").val(getToday())
+		$("select#field-license_id").val("");
+		$("select#field-license_id option[value='aafc-dsa']").hide();
+		$("#field-date_published").val(getToday())
         console.log("switched to open gov");
 }
 
